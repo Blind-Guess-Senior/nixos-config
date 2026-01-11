07415374b2d5934087a3139d3386743ca1d6cde1
Blind Guess Senior <Blind-Guess-Senior@outlook.com>
2026-01-11 13:16:28 +0800

    feat: binutils

--- configuration.nix
+++ configuration.nix
@@ -73,6 +73,28 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.libinput.enable = true;
 
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        # Shows battery charge of connected devices on supported
+        # Bluetooth adapters. Defaults to 'false'.
+        Experimental = true;
+        # When enabled other devices can connect faster to us, however
+        # the tradeoff is increased power consumption. Defaults to
+        # 'false'.
+        FastConnectable = true;
+      };
+      Policy = {
+        # Enable all controllers when they are found. This includes
+        # adapters present on start as well as adapters that are plugged
+        # in later on. Defaults to 'true'.
+        AutoEnable = true;
+      };
+    };
+  };
+
   programs.fish.enable = true;
   programs.zsh.enable = true;
 
@@ -96,6 +118,7 @@
       libclang
       gcc
       clang
+      binutils
     ];
   };
 
@@ -117,6 +140,7 @@
     tree
     git
     libarchive
+    nfs-utils
 
     zsh
     fish
