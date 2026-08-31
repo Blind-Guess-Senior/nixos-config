3e9a50dc412bd84da83c07e55110502fe8f20927
Blind Guess Senior <Blind-Guess-Senior@outlook.com>
2026-08-31 23:10:00 +0800

    feat: htpc746 system in settings

--- settings.nix
+++ settings.nix
@@ -2,6 +2,7 @@
   username = "blind-guess-senior";
   usernameUpper = "Blind-Guess-Senior";
   htpcHostName = "htpc746";
+  htpcSystem = "x86_64-linux";
   laptopHostName = "nix746";
   laptopSystem = "x86_64-linux";
   email = "Blind-Guess-Senior@outlook.com";
