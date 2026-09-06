03309022b3df7aad6f1c81d67ecfb9b968904658
Blind Guess Senior <94767867+Blind-Guess-Senior@users.noreply.github.com>
2026-09-06 18:58:52 +0800

    feat: fish plugins: done and fish-proxy

--- common/home/fish/default.nix
+++ common/home/fish/default.nix
@@ -1,8 +1,5 @@
 {
-  config,
   pkgs,
-  inputs,
-  outputs,
   ...
 }:
 
@@ -14,5 +11,22 @@
       "osupd" = "sudo nixos-rebuild switch --show-trace";
       "osdry" = "sudo nixos-rebuild dry-activate --show-trace";
     };
+
+    plugins = [
+      {
+        name = "done";
+        src = pkgs.fishPlugins.done.src;
+      }
+
+      {
+        name = "fish-proxy";
+        src = pkgs.fetchFromGitHub {
+          owner = "kaze-k";
+          repo = "fish-proxy";
+          rev = "f87e3c1f4e672daf0a954e24a765a49d1888bccb";
+          sha256 = "sha256-h8Oy+YeAyeI911NBYFqsqF93v5wg7o4ME4ZPUMPqptM=";
+        };
+      }
+    ];
   };
 }
