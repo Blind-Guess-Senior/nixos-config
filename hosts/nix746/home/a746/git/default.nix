5212184bc1e0400b66e6d53772008e1e9d83c05d
Blind Guess Senior <94767867+Blind-Guess-Senior@users.noreply.github.com>
2026-09-09 18:32:51 +0800

    feat: new git sign gpg key

--- hosts/nix746/home/a746/git/default.nix
+++ hosts/nix746/home/a746/git/default.nix
@@ -8,18 +8,18 @@
       user = {
         name = "Blind Guess Senior";
         email = "94767867+Blind-Guess-Senior@users.noreply.github.com";
-        # signingkey = "803C629090780959";
+        signingkey = "1B440AC3239E6C0BDC2032548B77B7825A272CB8!";
       };
 
-      # commit = {
-      #   gpgsign = true;
-      # };
-      # gpg = {
-      #   format = "openpgp";
-      # };
-      # tag = {
-      #   gpgSign = true;
-      # };
+      commit = {
+        gpgsign = true;
+      };
+      gpg = {
+        format = "openpgp";
+      };
+      tag = {
+        gpgSign = true;
+      };
 
       safe = {
         directory = [
