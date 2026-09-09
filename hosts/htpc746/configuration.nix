4deb3a2bc63d88c8256fd9aa4b357005bb133ee5
Blind Guess Senior <94767867+Blind-Guess-Senior@users.noreply.github.com>
2026-09-09 22:54:59 +0800

    feat: htpc ssh for 2222 port and fix substituter sequence

--- hosts/htpc746/configuration.nix
+++ hosts/htpc746/configuration.nix
@@ -23,10 +23,13 @@
     ];
 
     # Nix channels mirror.
-    extra-substituters = [
-      "https://nix-community.cachix.org"
+    substituters = [
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://cache.nixos.org/"
+    ];
+    extra-substituters = [
+      "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
--- hosts/htpc746/os-modules/openssh/default.nix
+++ hosts/htpc746/os-modules/openssh/default.nix
@@ -2,7 +2,7 @@
 {
   services.openssh = {
     enable = true;
-    ports = [ 22 ];
+    ports = [ 22 2222 ];
     settings = {
       PasswordAuthentication = false;
       PermitRootLogin = "no";
